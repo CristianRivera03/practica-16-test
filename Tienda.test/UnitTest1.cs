c17624d47dc43ada27c40ab62ecff2e0e218811f
Cristian RIvera <cr8539788@gmail.com>
2025-06-06 11:49:02 -0600

    iniciando practica

---
+++ Tienda.test/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using Tienda;
+
+namespace Tienda.test
+{
+    public class VentaTests
+    {
+        private Venta _venta;
+
+        [SetUp]
+        public void Setup()
+        {
+            _venta = new Venta();
+        }
+
+        [Test]
+        public void CalcularTotal_Cantidad2_Precio10_Retorna20()
+        {
+            double resultado = _venta.CalcularTotal(2, 10);
+            Assert.That(resultado, Is.EqualTo(100));
+        }
+
+        [Test]
+        public void CalcularTotal_Cantidad0_Retorna0()
+        {
+            double resultado = _venta.CalcularTotal(0, 50);
+            Assert.That(resultado, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void CalcularTotal_CantidadNegativa_LanzaExcepcion()
+        {
+            Assert.That(() => _venta.CalcularTotal(-1, 10), Throws.TypeOf<ArgumentException>());
+        }
+
+        [Test]
+        public void CalcularTotal_PrecioNegativo_LanzaExcepcion()
+        {
+            Assert.That(() => _venta.CalcularTotal(1, -5), Throws.TypeOf<ArgumentException>());
+        }
+    }
+}
---
+++ Tienda/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Tienda.test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var venta = new Venta();
+            double total = venta.CalcularTotal(3, 12.5);
+            Console.WriteLine($"Total a pagar: ${total}");
+        }
+    }
+
+    public class Venta
+    {
+        public double CalcularTotal(int cantidad, double precioUnitario)
+        {
+            if (cantidad < 0)
+                throw new ArgumentException("La cantidad no puede ser negativa");
+
+            if (precioUnitario < 0)
+                throw new ArgumentException("El precio unitario no puede ser negativo");
+
+            return Math.Round(cantidad * precioUnitario * 5, 2);
+        }
+    }
+}
